bd4fb8efe3f3728ce69f1395a88514306a30c673
Julian Safadi <julianmatiassafadi@hotmail.com>
2023-02-23 19:17:23 -0300

    [add] api coins

--- src/sections/main/Main.jsx
+++ src/sections/main/Main.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import CoinsTable from '../../components/CoinsTable';
 import './index.css'
 
 const Main = () => {
+
+  const getDate = () => {
+    const today = new Date()
+    const day = today.getDate()
+    const mounth = today.getMonth() + 1
+    const year = today.getFullYear()
+    const date = `${day}/${mounth}/${year}`
+    return date
+  }
+
   return (
     <section className="main__container">
     <div className="main__backgroundImg"></div>
@@ -12,29 +23,10 @@ const Main = () => {
     <section>
       <div className="main__table">
         <p className="main__table-title">Monedas principales</p>
-        <div className="main__table-container">
-          {/* <table>
-            <thead>
-              <td className="table__top-left">Bitcoin</td>
-              <td className="table__top-right table__right">$ 21.285 <span className="up"></span> </td>
-            </thead>
-            <thead>
-              <td>Ethereum</td>
-              <td className="table__right">$ 1.495 <span className="down"></span> </td>
-            </thead>
-            <thead>
-              <td>Binance Coin</td>
-              <td className="table__right">$ 326 <span className="down"></span> </td>
-            </thead>
-            <thead>
-              <td className="table__bottom-left">Solana</td>
-              <td className="table__bottom-right table__right">$ 28 <span className="down"></span> </td>
-            </thead>
-          </table> */}
-        </div>
+          <CoinsTable/>
       </div>
       <div className="main__table-date">
-        <p> <b>Actualizado:</b> 17 Julio 21:00 </p>
+        <p> <b>Actualizado:</b> {getDate()} </p>
       </div>
       <div>
       </div>
